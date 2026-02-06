b7fc528b1afb5863e8c392fff5f0437fe8825252
João Pedro Araújo <joao.pedrocdearaujo@gmail.com>
2026-02-06 00:23:35 -0300

    style(Goals): resize elements and adjust the layout

--- src/Components/Goals/Goals.tsx
+++ src/Components/Goals/Goals.tsx
@@ -1,117 +1,129 @@
 import { GoalsProps } from "./Goals.type";
 import './Goals.css';
 
-const GoalsCard = ({cost, date, img, name} : GoalsProps) => {
+
+const goals = [
+  {
+    id: 1,
+    cost: '550',
+    date: '12/20/20',
+    img: './public/holidays.png',
+    name: 'Holidays',
+  },
+  {
+    id: 2,
+    cost: '1200',
+    date: '05/15/21',
+    img: './public/renovation.png',
+    name: 'Home Renovation',
+  },
+  {
+    id: 3,
+    cost: '3500',
+    date: '08/10/21',
+    img: './public/xbox.png',
+    name: 'Xbox Series X',
+  },
+  {
+    id: 4,
+    cost: '800',
+    date: '01/12/22',
+    img: './public/holidays.png',
+    name: 'Beach Trip',
+  },
+  {
+    id: 5,
+    cost: '2500',
+    date: '03/20/22',
+    img: './public/renovation.png',
+    name: 'Kitchen Setup',
+  },
+  {
+    id: 6,
+    cost: '500',
+    date: '06/05/22',
+    img: './public/xbox.png',
+    name: 'New Games',
+  },
+  {
+    id: 7,
+    cost: '1500',
+    date: '09/15/22',
+    img: './public/holidays.png',
+    name: 'Winter Break',
+  },
+  {
+    id: 8,
+    cost: '4000',
+    date: '11/30/22',
+    img: './public/renovation.png',
+    name: 'Bedroom Remodel',
+  },
+  {
+    id: 9,
+    cost: '450',
+    date: '02/14/23',
+    img: './public/xbox.png',
+    name: 'Xbox Controller',
+  },
+  {
+    id: 10,
+    cost: '2000',
+    date: '04/10/23',
+    img: './public/holidays.png',
+    name: 'Euro Summer',
+  }
+]
+
+const GoalsItem = ({cost, date, img, name} : GoalsProps) => {
     return (
-        <div className="goals__card">
-            <div className="goals__values">
-                <h1 className="goals__cost">{`$${cost}`}</h1>
-                <h3 className="goals__date">{`${date}`}</h3>
+        <div className="item">
+            <div className="item__values">
+                <h1 className="item__cost">{`$${cost}`}</h1>
+                <h3 className="item__date">{`${date}`}</h3>
             </div>
-            <div className="goals__label">
-                <img src={`${img}`} alt="Goal Image" className="goals__icon" />
-                <h2 className="goals__name">{name}</h2>
+            <div className="item__label">
+                <img src={`${img}`} alt="Goal Image" className="item__icon"/>
+                <h2 className="item__name">{name}</h2>
             </div>
         </div>
     )   
 }
 
 const Goals = () => {
-    const goals = [
-        {
-        id: 1,
-        cost: '550',
-        date: '12/20/20',
-        img: './public/holidays.png',
-        name: 'Holidays',
-        },
-        {
-        id: 1,
-        cost: '550',
-        date: '12/20/20',
-        img: './public/holidays.png',
-        name: 'Holidays',
-        },
-        {
-        id: 1,
-        cost: '550',
-        date: '12/20/20',
-        img: './public/holidays.png',
-        name: 'Holidays',
-        },
-        {
-        id: 1,
-        cost: '550',
-        date: '12/20/20',
-        img: './public/holidays.png',
-        name: 'Holidays',
-        },
-        {
-        id: 1,
-        cost: '550',
-        date: '12/20/20',
-        img: './public/holidays.png',
-        name: 'Holidays',
-        },
-        {
-        id: 1,
-        cost: '550',
-        date: '12/20/20',
-        img: './public/holidays.png',
-        name: 'Holidays',
-        },
-        {
-        id: 1,
-        cost: '550',
-        date: '12/20/20',
-        img: './public/holidays.png',
-        name: 'Holidays',
-        },
-        {
-        id: 2,
-        cost: '200',
-        date: '12/20/20',
-        img: './public/renovation.png',
-        name: 'Renovation',
-        },
-        {
-        id: 3,
-        cost: '820',
-        date: '12/20/20',
-        img: './public/xbox.png',
-        name: 'Xbox',
-        },
-        {
-        id: 3,
-        cost: '820',
-        date: '12/20/20',
-        img: './public/xbox.png',
-        name: 'Xbox',
-        },
-    ]
-    
+       
     return(
-        <div className="goals-container">
+        <div className="goals">
             <div className="goals__header">
                 <p className="goals__title">Goals</p>
-                <button className="goals__add-goal">
+                <button className="goals__add-goal"
+
+                >
                     <img src="./public/add.png" alt="button"className="goals__add-icon" />
                 </button>
             </div>
-            <div className="goals__wrapper">
-                <div className="goals__card-container">
-                    {goals.map((goal, key) => (
-                        <GoalsCard
-                        id={goal.id}
-                        key={goal.id}
-                        cost={goal.cost}
-                        date={goal.date}
-                        img={goal.img}
-                        name={goal.name}
-                        />
-                    ))}
+            
+                <div className="goals__wrapper">
+                    <button className="goals__button goals__button--previous"
+                            aria-label="Next Item"
+                    >
+                       <img src="/left-arrow.png" alt="" aria-hidden="true" className="goals__arrow-icon"/>
+                    </button>
+
+                    <div className="goals__item-container" >
+                        {goals.map((goal, key) => (
+                            <GoalsItem key={goal.id} {...goal}
+                            />
+                            
+                        ))}
+                    </div>
+
+                    <button className="goals__button goals__button--next"
+                            aria-label="Next Item"
+                    >
+                       <img src="/right-arrow.png" alt="" aria-hidden="true" className="goals__arrow-icon"/>
+                    </button>
                 </div>
-            </div>
         </div>
     )
 }
